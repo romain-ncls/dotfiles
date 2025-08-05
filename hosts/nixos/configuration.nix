92ddbe13296697a6cade7ab4092d0e040fcf4316
Romain Nicolas <nicolasromain0@gmail.com>
2025-08-05 12:00:09 +0200

    chore: update linux kernel to 6.16

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -29,7 +29,7 @@ in
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  boot.kernelPackages = pkgs.linuxPackages_6_15;
+  boot.kernelPackages = pkgs.linuxPackages_6_16;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
