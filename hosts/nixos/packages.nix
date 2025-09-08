43f52338aac3562c8822d3865652e89c91735032
Romain Nicolas <nicolasromain0@gmail.com>
2025-09-08 14:27:33 +0200

    feat(packages): qtwebengine

--- hosts/nixos/packages.nix
+++ hosts/nixos/packages.nix
@@ -60,6 +60,7 @@ in
     ffmpeg # Used for conversion to mono in the soundbox
     xdotool # Used for the "focus discord" shortcut
 
+    kdePackages.qtwebengine # needed by HTML wallpaper
     konsave # KDE settings exporter.
 
     git
